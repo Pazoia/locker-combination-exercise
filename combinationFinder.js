d7d3c3ae4b563ae01dc0e82cc939e634ea8f083d
Paulo Azoia <pazoia.gomes@gmail.com>
2021-12-17 12:14:14 +0000

    Hint 2 returning pairs were the second element is divisible by three

--- combinationFinder.js
+++ combinationFinder.js
@@ -1,6 +1,6 @@
 function hintSolver() {
     // Hint 1
-    // // The product of the first two digits is 24.
+    // The product of the first two digits is 24.
     const product = 24;
     let possibleCombinations = [];
     for (let digit1 = 0; digit1 < 10; digit1++) {
@@ -11,6 +11,14 @@ function hintSolver() {
         }
     }
     console.log("Hint 1");
+    console.log(possibleCombinations);
+
+    // Hint 2
+    // The fourth digit is one-third of the second digit.
+    possibleCombinations = possibleCombinations.filter(([first, second]) => {
+        return second % 3 === 0;
+    });
+    console.log("Hint 2");
     console.log(possibleCombinations);
 
     console.log(`Your secret code is: `);
